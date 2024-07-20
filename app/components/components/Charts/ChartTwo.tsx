3344a877db713563824df2a00a3e15496f37f9d9
mungaben <kamaumungai3742@gmail.com>
2024-07-20 11:03:24 -0700

    importsxxxxx

--- app/components/components/Charts/ChartTwo.tsx
+++ app/components/components/Charts/ChartTwo.tsx
@@ -82,8 +82,6 @@ const ChartTwo: React.FC = () => {
 
       markers: {
         radius: 99,
-        width: 16,
-        height: 16,
         strokeWidth: 10,
         strokeColor: "transparent",
       },
